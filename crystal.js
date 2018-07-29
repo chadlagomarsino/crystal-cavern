ef3ff33bb6e7ecebd4fe9f4be3c613d533692815
Chad Lagomarsino <chadlagomarsino74@gmail.com>
2018-07-29 15:48:41 -0500

    Improved Modules, Object Generation drafted.

--- crystal.js
+++ crystal.js
@@ -9,14 +9,13 @@
   //around them
   //Each crystal time has special properties to put on the prototype
 
-//Initalize creation of blank table and divs
-//Event Click to create instance of Crystal
-
 const boardModule = (function board() {
-  //default is a 4 by 4 square of divs
+  // default is a 4 by 4 square of divs
   let numDiv = [16];
-  //two dimensional array to store position and color of clicked crystals
-  let positionAndColor = [];
+
+  // array of color and position values for active crystals from the
+  // crystalsModule.crystalsToRender array of objects
+  let crystalsToActivate = [];
 
   //cache DOM
   const $container = $('#container');
@@ -30,107 +29,181 @@ const boardModule = (function board() {
   //bind events
   $reload.on('click', function() {
     if ($userInput.val() >= 4 && $userInput.val() <= 64) {
-      render();
+      _render();
     }
     else {console.log("Input must be greater than 4 and less than 64.");}
   });
-  //TODO start
+  $start.one('click', function() {
+    crystalsModule.crystalsModuleInit();
+  });
   //TODO reset
-  refreshDivs();
+  _refreshDivs();
 
-  function refreshDivs() {
+  function boardModuleInit() {
+    //access from crystal array
+    return;
+  }
+
+  function _refreshDivs() {
     //grab all divs
     let $divs = $board.find('.item');
     // enable color change on click
     $divs.one('click', function() { this.style.background = 'tomato'});
     // track which divs were clicked
-    $divs.one('click', logPosition);
+    $divs.one('click', _logPosition);
   }
 
   //log position of activated divs
-  function logPosition() {
+  function _logPosition() {
+    let color = "red"
+    //NOTE: Position IDs begin with 0 and increment untill the number of
+    // squares - 1.
     let position = this.id;
-    let color = "Red"
-    positionAndColor.push([position, color]);
-    console.log(positionAndColor);
+    crystalsToActivate.push([color, position]);
   }
 
   //remove all divs from game board
-  function clearBoard() {
+  function _clearBoard() {
     $board.empty();
   }
 
   //update numDiv and --colNum for CSS Grid with user input
-  function setInput() {
+  function _setInput() {
     numDiv.pop();
     let inputValue = $userInput.val();
     numDiv.push(inputValue * inputValue);
     $board.css('--colNum', inputValue);
   }
 
-  function render() {
-    clearBoard();
-    setInput();
+  function _render() {
+    _clearBoard();
+    _setInput();
     for (let i = 0; i < numDiv[0]; i++) {
       $board.append(`<div id="${i}" class="item"></div>`);
     };
-    refreshDivs();
+    _refreshDivs();
   }
 
   return {
-    //pass ACTIVATED divs to crystalModule
-    positionAndColor: positionAndColor
+    numDiv: numDiv,
+    boardModuleInit: boardModuleInit,
+    crystalsToActivate: crystalsToActivate,
   };
 })();
 
-const crystals = {
-  init: function() {
-    this.generateCrystal(true, "Red");
-    console.log(this.redCrystals)
-  },
-  //Store Crystal Constructors
-  generateCrystal: function(position, color) { //TODO add color parameter
-    //Crystal Parent Constructor
-    function ParentCrystal (position) {
+const crystalsModule = (function crystals() {
+
+  //An a array of crystal objects built from the color and position
+  //values recieved from the boardModule.crystalsToActivate array
+  let crystalsToRender = [];
+
+  //Crystal Objects
+  const Crystal = {
+    init: function(color, position) {
+      this.color = color,
       this.position = position,
       this.willGrow = false,
       this.willDie = false,
-      this.Neighbors = []
-    };
-    ParentCrystal.prototype = {
-      getNeighbors: function() {
-        //what kind of piece am I?
-        //scan position and color Array from gameBoardAPI for neighbors
-        //Log Neighbors
+      this.pieceType = "central",
+      this.neighbors = []
+    },
+    setPieceType: function(boardLength) {
+      //what kind of piece am I?
+      if (this.position == 0) {
+        //NW corner
+        this.pieceType = "nw corner";
         return;
-      },
-      setGrow: function() {
-        this.willGrow = true;
-      },
-      setDie: function() {
-        this.willDie = true;
       }
+      else if (this.position == (boardLength - 1)) {
+        //NE corner
+        this.pieceType = "ne corner";
+        return;
+      }
+      else if (this.position == ((boardLength*boardLength) - 1)) {
+        //SE corner
+        this.pieceType = "se corner";
+        return;
+      }
+      else if (this.position == ((boardLength*boardLength) - boardLength)) {
+        //SW corner
+        this.pieceType = "sw corner";
+        return;
+      }
+      else if (this.position % boardLength == 0) {
+        //West Edge
+        this.pieceType = "west edge";
+        return;
+      }
+      else if ((this.position) % (boardLength) == (boardLength - 1)) {
+        //East Edge
+        this.pieceType = "east edge";
+        return;
+      }
+      else if ((this.position > 0) && (this.position < (boardLength - 1))) {
+        //North Edge
+        this.pieceType = "north edge";
+        return;
+      }
+      else if ((this.position > ((boardLength*boardLength) - boardLength)) &&
+      (this.position < (boardLength*boardLength))) {
+        //South Edge
+        this.pieceType = "south edge";
+        return;
+      }
+      else {
+        //central position
+        this.pieceType = "central";
+        return;
+      }
+    },
+    setGrow: function() { this.willGrow = true; },
+    setUnGrow: function() { this.willGrow = false; },
+    setDie: function() { this.willDie = true; },
+    setUnDie: function() { this.willDie = false; },
+    findNeighbors: function() {
+      switch (this.pieceType) {
+        case: "nw corner"
+          //search crystal to render for crystals in position
+          // log color and position and in neighbors
+          // use filter
+      }
+      return
+    }
+  };
+
+  function crystalsModuleInit() {
+    //recieve active crystal list from boardModule.
+    //index 0 = position, index 1 = color
+    let crystalsToActivate = boardModule.crystalsToActivate;
+    generateCrystal(crystalsToActivate);
+  };
+
+  function generateCrystal(crystalsToActivate) {
+    let boardLength = Math.sqrt(boardModule.numDiv);
+    console.log(crystalsToActivate);
+    for(let i = 0; i < crystalsToActivate.length; i++) {
+      crystalsToRender[i] = Object.create(Crystal);
+      crystalsToRender[i].init("red", crystalsToActivate[i][1]);
+      crystalsToRender[i].setPieceType(boardLength);
+      //TIME TO WHIP OUT SOME FUNCTIONAL PROGRAMMING AND
+      // Grow crystals by passing back crystalsToRender module
     };
-    //Child Crystal Contructors
-    switch (color) {
-      case "Red":
-        function RedCrystal (position) {
-          ParentCrystal.call(this, position);
-          this.color = "Red";
-        };
-        RedCrystal.prototype = Object.create(ParentCrystal.prototype);
-        RedCrystal.prototype.redTest = function() {
-          return "REDTEST";
-        };
-        RedCrystal.prototype.constructor = RedCrystal;
-        for(let i = 0; i < 5; i++) {
-          const aRedCrystal = new RedCrystal(i);
-          this.redCrystals.push(aRedCrystal);
-          //console.log(aRedCrystal);
-        }
-        break;
-      case "Blue":
-        break;
-    }
-    }
-};
+  };
+
+  function changeCrystalState() {
+    return;
+  }
+
+  function changeCrystalsToActivate() {
+    return;
+  }
+
+  function returnCrystals() {
+    return;
+  }
+
+  return {
+    crystalsModuleInit: crystalsModuleInit,
+    crystalsToRender: crystalsToRender,
+  };
+})();
